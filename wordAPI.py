a2128817ad4f5df79f65e22fb61a789b0b759c03
marcus810 <marcus@jensenpost.dk>
2020-02-17 13:54:53 +0100

    modulet kan hente requests fra api'en

--- wordAPI.py
+++ wordAPI.py
@@ -1,13 +1,24 @@
 import requests
 
+listemedord = ["hate", "love", "gluttony"]
 
-def api():
-    wList = open("wordsynonym.txt")
-    print(wList.readlines())
-    wList.close()
+def api(list):
+ #ord der skal findes synonym for skal sidde mellem words og synynoms
+ #eksempel "https://wordsapiv1.p.rapidapi.com/words/lovely/synonyms" her findes synynom til lovely
+    url = "https://wordsapiv1.p.rapidapi.com/words/synonyms"
+
+
+    headers = {
+        'x-rapidapi-host': "wordsapiv1.p.rapidapi.com",
+        'x-rapidapi-key': "7617a202a7msh9f33d595f3be64ap1c8538jsnbf77c9af8318"
+        }
+
+    response = requests.request("GET", url, headers=headers)
+
+    print(response.text)
 
 
 
-#append "wordsynonym.txt" til liste og herefter hent synonymer fra wordapi
-#lav synonymer til en liste.
-api()
+
+
+api(listemedord)
