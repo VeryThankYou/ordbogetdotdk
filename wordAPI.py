bb2fd89eff2b252caf307d2825d79258ff4ee39a
marcus810 <marcus@jensenpost.dk>
2020-03-01 14:56:11 +0100

    Done

--- wordAPI.py
+++ wordAPI.py
@@ -1,12 +1,27 @@
-import requests
+import requests, json
 
-
-def api(list):
+def api(liste):
  #ord der skal findes synonym for skal sidde mellem words og synynoms
  #eksempel "https://wordsapiv1.p.rapidapi.com/words/lovely/synonyms" her findes synynom til lovely
+    synonyms = []
+    wordchoice = []
     headers = {
         'x-rapidapi-host': "wordsapiv1.p.rapidapi.com",
         'x-rapidapi-key': "7617a202a7msh9f33d595f3be64ap1c8538jsnbf77c9af8318"
         }
-    for i in list:
+    for i in liste:
         response = requests.request("GET", "https://wordsapiv1.p.rapidapi.com/words/" + i + "/synonyms", headers=headers)
+        #laver json om til en dict, så den kan bruges i python
+        data = json.loads(response.text)
+
+        try:
+#da value nummer 1 er en liste af synonymer, skal vi have fat i value nummer 0
+#til listen som ligger i value nummer 1 for at få det første synonym i listen
+            fsynonym = list(list(data.values())[1])[0]
+            fword = list(data.values())[0]
+#da nogen ord ikke har synonymer har den ikke nogen index nummer 0
+#derfor vil programmet stoppe og fortælle der er en fejl. Det ignorer vi.
+        except IndexError:
+            continue
+        wordchoice.append(fword)
+        synonyms.append(fsynonym)
